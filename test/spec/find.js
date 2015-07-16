498ad8a739f0c1bcdd2929dda0293c89910af6bc
muji <noop@xpm.io>
2015-07-16 12:26:39 +0800

    Initial assertions.

--- test/spec/find.js
+++ test/spec/find.js
@@ -4,11 +4,16 @@ var expect = require('chai').expect
 describe('fs-find:', function() {
 
   it('should find files', function(done) {
-    find([process.cwd() + '/test/fixtures/mock'], function(err, files) {
+    var base = 'test/fixtures/mock';
+    find([base], function(err, files) {
       if(err) {
         return done(err) ;
       }
-      console.dir(files) 
+      var map = find.map(files);
+      expect(map[base + '/mock.txt']).to.be.an('object');
+      expect(map[base + '/empty.txt']).to.be.an('object');
+      expect(map[base + '/deep/alt.txt']).to.be.an('object');
+      expect(map[base + '/deep/alt-file.txt']).to.be.an('object');
       done();
     })
   });
