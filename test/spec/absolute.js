146bc0ad65c071c63c69f277c9c3ac9f1e4eef85
muji <freeformsystems@gmail.com>
2016-01-18 13:09:06 +0800

    Add absolute test spec.

---
+++ test/spec/absolute.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect
+  , path = require('path')
+  , find = require('../../lib')
+  , base = 'test/fixtures/mock';
+
+describe('fs-find:', function() {
+
+  it('should find files w/ string path', function(done) {
+    var opts = {absolute: true, dedupe: true};
+    find(base, opts, function(err, files) {
+      if(err) {
+        return done(err);
+      }
+      var map = find.map(files)
+        , pth = path.join(process.cwd(), base);
+      expect(map[pth + '/mock.txt']).to.be.an('object');
+      expect(map[pth + '/empty.txt']).to.be.an('object');
+      expect(map[pth + '/deep/alt.txt']).to.be.an('object');
+      expect(map[pth + '/deep/alt-file.txt']).to.be.an('object');
+      done();
+    })
+  });
+
+});
